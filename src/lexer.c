036c38fcbae5ecc6156fbd68d074b5028a774bed
Thomas Joubert <thomas.joubert@epita.fr>
2022-02-19 00:22:04 +0100

    feat(lexer): add token enum

--- src/lexer.c
+++ src/lexer.c
@@ -1,4 +1,4 @@
-#include "interpretor.h"
+#include "lexer.h"
 
 char memory[30000] = { 0 };
 
@@ -13,16 +13,44 @@ char available_tokens[8] = {
     ']'
 };
 
-int lexer(FILE *code, int *cursor)
+static enum token map_char(int character)
+{
+    switch (character)
+    {
+        case '>':
+            return PTR_INC;
+        case '<':
+            return PTR_DEC;
+        case '+':
+            return VAL_INC;
+        case '-':
+            return VAL_DEC;
+        case '.':
+            return PRINT;
+        case ',':
+            return GETC;
+        case '[':
+            return LOOP_START;
+        case ']':
+            return LOOP_END;
+        default :
+            return EOF_TOK;
+    }
+
+    return EOF_TOK;
+}
+
+enum token lexer(FILE *code, int *cursor)
 {
     if (fseek(code, (*cursor)++, SEEK_SET) == -1)
         err(3, "Error reading code");
 
     int character = fgetc(code);
     if (is_character_valid(character))
-        return character;
+        return map_char(character);
 
-    errx(4, "Unrecognized token \'%c\' at character %d", (char)character, *cursor);
+    errx(4, "Unrecognized token \'%c\' at character %d", (char)character,
+            *cursor);
 }
 
 int is_character_valid(const int character)
--- src/lexer.h
+++ src/lexer.h
@@ -6,8 +6,21 @@
 
 #define TOKEN_NUMBER 8
 
+enum token
+{
+    PTR_INC = 0,     // >
+    PTR_DEC,         // <
+    VAL_INC,         // +
+    VAL_DEC,         // -
+    PRINT,           // .
+    GETC,            // ,
+    LOOP_START,      // [
+    LOOP_END,        // ]
+    EOF_TOK          // EOF
+};
+
 int is_character_valid(const int character);
 
-int lexer(FILE *code, int *cursor);
+enum token lexer(FILE *code, int *cursor);
 
 #endif // end of DEF_INTERPRETOR
