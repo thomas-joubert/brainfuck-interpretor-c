e30903c0094d1a4c5ad2de09f0a9fa3eecbd9b28
Thomas Joubert <thomas.joubert@epita.fr>
2022-02-19 13:44:55 +0100

    feat(tests): add eval tests

--- tests/testsuite.sh
+++ tests/testsuite.sh
@@ -2,7 +2,7 @@
 passed=0
 failed=0
 
-testcase () {
+synatx_testcase () {
     ./../interpretor $1 2> log.err
 
     if [[ `cat log.err` = "$2" && $? = $3 ]]; then
@@ -21,11 +21,36 @@ testcase () {
     fi
 }
 
-testcase "syntax/bad/first_character_bad_token.brainfuck" "Unrecognized token \'a\' at character 1" 4
+eval_testcase () {
+    echo $1 | ./../interpretor $2 2> log.err
 
-testcase "syntax/bad/last_character_bad_token.brainfuck" "Unrecognized token \'a\' at character 18" 4
+    if [[ `cat log.err` = "$3" && $? = $4 ]]; then
 
-testcase "syntax/good/simple.brainfuck" "" 0
+        echo $3 > tmp
+
+        echo "Output does not match expected"
+        diff log.err tmp
+
+        rm log.err tmp
+
+        failed=$(("$failed" + 1))
+    else
+        passed=$(("$passed" + 1))
+        rm log.err
+    fi
+}
+
+exec
+
+synatx_testcase "syntax/bad/first_character_bad_token.brainfuck" "Unrecognized token \'a\' at character 1" 4
+
+synatx_testcase "syntax/bad/last_character_bad_token.brainfuck" "Unrecognized token \'a\' at character 18" 4
+
+synatx_testcase "syntax/good/simple.brainfuck" "" 0
+
+eval_testcase "" "eval/hello_world.brainfuck" "Hello World!" 0
+
+eval_testcase 'a' "eval/lower_case.brainfuck" "A" 0
 
 echo "passed : $passed"
 echo "failed : $failed"
